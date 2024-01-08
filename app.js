33a8621a01f28efba0785c85770b9d83c733fb49
DenverDen <denverden@mail.ru>
2024-01-08 15:28:00 +0300

    fix: no use tags selector according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -29,19 +29,23 @@ var createNewTaskElement = function (taskString) {
   var deleteButton = document.createElement("button"); //delete button
   var deleteButtonImg = document.createElement("img"); //delete button image
 
+  listItem.className = "todo-item";
+
   label.innerText = taskString;
-  label.className = "task";
+  label.className = "task todo-label";
 
   //Each elements, needs appending
   checkBox.type = "checkbox";
+  checkBox.className = "todo-checkbox";
   editInput.type = "text";
-  editInput.className = "task";
+  editInput.className = "task todo-input";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "edit";
+  editButton.className = "btn edit";
 
-  deleteButton.className = "delete";
+  deleteButton.className = "btn delete";
   deleteButtonImg.src = "./remove.svg";
+  deleteButtonImg.className = "icon";
   deleteButton.appendChild(deleteButtonImg);
 
   //and appending.
