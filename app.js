a2c786d7a3f4ae725e2213332465426223d6ab79
DenverDen <denverden@mail.ru>
2024-01-08 19:33:26 +0300

    fix: adjust alternate content for multimedia according to rule 1.2 (Semantic) in html-and-css-extended.md

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ var createNewTaskElement = function (taskString) {
   deleteButton.className = "btn delete";
   deleteButtonImg.src = "./remove.svg";
   deleteButtonImg.className = "icon";
+  deleteButtonImg.setAttribute("alt", "button delete");
   deleteButton.appendChild(deleteButtonImg);
 
   // and appending.
